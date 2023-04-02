73933055d9c2449dcfa8a189c3b4ed5ba8d76ec8
Brenda <brendakajuju777@gmail.com>
2023-04-02 13:19:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+// // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//     },
+// body: JSON.stringify( {formData})
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then (function(response) {
+// return response.json();
+// })
+// .then(function (object) {
+// console.log(object);
+// });
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+body: JSON.stringify(formData)
+};
+
+function submitData() {
+ fetch("http://localhost:3000/users", configurationObject)
+.then (function(response) {
+return response.json();
+})
+.then(function (object) {
+const id = object.id
+document.body.innerHTML += id})
+.catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);})
+}
+submitData ();
